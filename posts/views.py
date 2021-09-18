23d8789b0ed514b2b4e1f659305e42c9480cd27d
Your Name <you@example.com>
2021-09-18 12:05:54 +0200

    before unit tests

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
 from posts.models import Post
 from posts.serializers import PostRetrieveSerializer, PostCreateSerializer
-
+from rest_framework import status
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 # Create your views here.
+from rest_framework.response import Response
+
+
 
 class PostViewSet(ModelViewSet):
     serializer_class = PostCreateSerializer
@@ -22,3 +25,10 @@ class PostViewSet(ModelViewSet):
             return PostRetrieveSerializer
         return PostCreateSerializer
 
+    def create(self, request, *args, **kwargs):
+        request.data.update({'author': request.user.pk})
+        serializer = self.get_serializer(data=request.data, author=request.user)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
