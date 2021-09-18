3d4e2f49d63be6a88cdcd0267189c52bc4d111ca
Your Name <you@example.com>
2021-09-18 11:50:27 +0200

    serializers, adding posts using api

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,14 +1,29 @@
 from rest_framework import serializers
 
 from posts.models import Post
+from users.serializers import UserSerializer
 
 
-class PostSerializer(serializers.ModelSerializer):
+class PostRetrieveSerializer(serializers.ModelSerializer):
+
+    author = UserSerializer()
+
     class Meta:
         model = Post
         fields = (
             'id',
             'title',
             'content',
+            'author',
+        )
 
+
+class PostCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = (
+            'id',
+            'title',
+            'content',
+            'author'
         )
--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,24 @@
 from rest_framework.viewsets import ModelViewSet
 from posts.models import Post
-from posts.serializers import  PostSerializer
+from posts.serializers import PostRetrieveSerializer, PostCreateSerializer
 
-
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 # Create your views here.
 
 class PostViewSet(ModelViewSet):
-    serializer_class = PostSerializer
+    serializer_class = PostCreateSerializer
     queryset = Post.objects.all()
     # permission_classes = [IsAdminUser]
+
+    def get_permissions(self):
+        #print(self.action)
+        if self.action in {'list', 'retrieve'}:
+            return []
+
+        return [IsAdminUser()]
+
+    def get_serializer_class(self):
+        if self.action in {'list', 'retrieve'}:
+            return PostRetrieveSerializer
+        return PostCreateSerializer
+
